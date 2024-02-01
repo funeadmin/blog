d2028ef79cc81de79d6b1418da88386225d9d554
Lupe Villalpando <funemone@gmail.com>
2024-02-01 09:10:01 -0800

    added create, update

--- flask/shout/src/api/users.py
+++ flask/shout/src/api/users.py
@@ -60,32 +60,51 @@ def delete(id: int):
         # something went wrong :(
         return jsonify(False)
 
-
-@bp.route('/<int:id>', methods=['PATCH', 'PUT'])
-def update(id: int):
-    u = User.query.get_or_404(id)
-
+@bp.route('', methods=['POST'])
+def create():
     # req body must contain username and password
     if 'username' not in request.json and 'password' not in request.json:
         return abort(400)
-    if len('username') >= 3 in request.json:
-        u.username = request.json['username']
+    if len('username') <= 3 or len('password') <= 8 in request.json:
+        return abort(400)
+    
+    # contstruct User 
+    u = User(
+        username=request.json['username'],
+        password=scramble(request.json['password'])    # scramble password being passed
+    )
+    
+    db.session.add(u) # prepare CREATE statement
+    db.session.commit() # execute CREATE statement
+    return jsonify(u.serialize())
+    
+@bp.route('</init:id>', methods=['PATCH','PUT'])
+def update():
+    u = User.query.get_or_404(id)   
+    # req body must contain username and password
+    if 'username' not in request.json and 'password' not in request.json:
+        return abort(400)
+    if len('username') >= 3 in request.json: 
+        username=request.json['username']
         return u.username
     if len('password') >= 8 in request.json:
-        u.password = scramble(request.json['password'])
-        return u.password
-    else:
-        abort(400)
-
+        password=scramble(request.json['password'])    
+        return u.password 
+    else: 
+        abort(400)   
+   # contstruct User 
+    u = User(
+        username=request.json['username'],
+        password=scramble(request.json['password'])    # scramble password being passed
+    ) 
     try:
-        db.session.add(u)  # prepare CREATE statement
-        db.session.commit()  # execute CREATE statement
-        return jsonify(u.serialize())
-    except:
+       db.session.add(u) # prepare CREATE statement
+       db.session.commit() # execute CREATE statement
+       return jsonify(u.serialize())  
+    except: 
         # something went wrong :(
         return jsonify(False)
-
-
+    
 @bp.route('/<int:id>/liked_shouts', methods=['GET'])
 def liked_shouts(id: int):
     u = User.query.get_or_404(id)
